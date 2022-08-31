13cfcc3f47c704ce9ffef58fc2d9e17ae51a9aa8
Pedro Barbieri <pedro.romaguera@hotmail.com>
2022-08-30 21:46:21 -0300

    First calc react

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { Button } from './components/Button';
 
 export default function App() {
   const [operation, setOperation] = useState("");
-  const [firstNum, setFirstNum] = useState("0");
+  const [firstNum, setFirstNum] = useState("");
   const [secondNum, setSecondNum] = useState("");
 
   function handleOperation(operation: string){
@@ -12,7 +12,38 @@ export default function App() {
   }
 
   function handleNumberPress (number: string) {
+    if(firstNum.length < 11){
+      if(firstNum === "0") {
+        setFirstNum(number);
+    } else {
+      setFirstNum(firstNum + number);
+    }
+  }
+}
+
+  function getFirstNum(){
+    if(!firstNum){
+      return <Text style={styles.displayFirstNum}>0</Text>
+    }
+
+    if(firstNum.length > 6 && firstNum.length <= 8){
+      return  <Text style={[styles.displayFirstNum, {fontSize: 70}]}>{firstNum}</Text>
+    }
+      if (firstNum.length > 8){
+        return <Text style={[styles.displayFirstNum, {fontSize: 50}]}>{firstNum}</Text>    
+    }
     
+    return  <Text style={styles.displayFirstNum}>{firstNum}</Text>
+  }
+
+  function handleBackspace(){
+    setFirstNum(firstNum.slice(0, -1));
+  }
+
+  function handleClearPress(){
+    setFirstNum("");
+    setSecondNum("");
+    setOperation("");
   }
 
   return (
@@ -21,36 +52,36 @@ export default function App() {
         <Text style={styles.displaySecondNum}>
           {secondNum} <Text style={styles.displayOperation}>{operation}</Text> 
         </Text>
-        <Text style={styles.displayFirstNum}>{firstNum}</Text>
+       {getFirstNum()}
       </View>
       <View style={styles.row}>
-        <Button text="AC" onPress={() => {}} isGray/>
+        <Button text="AC" onPress={() => handleClearPress()} isGray/>
         <Button text="+/-" onPress={() => handleOperation("+/-")} isGray/>
         <Button text="%" onPress={() => handleOperation("%")} isGray/>
         <Button text="÷" onPress={() => handleOperation("÷")} isOrange/>
       </View>
       <View style={styles.row}>
-        <Button text="7" onPress={() => {}} />
-        <Button text="8" onPress={() => {}} />
-        <Button text="9" onPress={() => {}} />
+        <Button text="7" onPress={() => handleNumberPress("7")} />
+        <Button text="8" onPress={() => handleNumberPress("8")} />
+        <Button text="9" onPress={() => handleNumberPress("9")} />
         <Button text="x" onPress={() => handleOperation("x")} isOrange/>
       </View>
       <View style={styles.row}>
-        <Button text="4" onPress={() => {}} />
-        <Button text="5" onPress={() => {}} />
-        <Button text="6" onPress={() => {}} />
+        <Button text="4" onPress={() => handleNumberPress("4")} />
+        <Button text="5" onPress={() => handleNumberPress("5")} />
+        <Button text="6" onPress={() => handleNumberPress("6")} />
         <Button text="-" onPress={() => handleOperation("-")} isOrange/>
       </View>
       <View style={styles.row}>
-        <Button text="1" onPress={() => {}} />
-        <Button text="2" onPress={() => {}} />
-        <Button text="3" onPress={() => {}} />
+        <Button text="1" onPress={() => handleNumberPress("1")} />
+        <Button text="2" onPress={() => handleNumberPress("2")} />
+        <Button text="3" onPress={() => handleNumberPress("3")} />
         <Button text="+" onPress={() => handleOperation("+")} isOrange/>
       </View>
       <View style={styles.row}>
-        <Button text="0" onPress={() => {}} />
-        <Button text="," onPress={() => {}} />
-        <Button text="." onPress={() => {}} />
+        <Button text="." onPress={() => handleNumberPress(".")} />
+        <Button text="0" onPress={() => handleNumberPress("0")} />
+        <Button text="⌫" onPress={() => handleBackspace()} />
         <Button text="=" onPress={() => handleOperation("=")} isOrange/>
       </View>
     </View>
